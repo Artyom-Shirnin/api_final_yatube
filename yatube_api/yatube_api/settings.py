32d64500505887594057e2d30eb26f8a4ad81c39
Артём Ширнин <artyom.shirnin@yandex.ru>
2022-07-26 22:19:04 +1000

    32 из 33

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -100,6 +100,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-   'AUTH_HEADER_TYPES': ('Bearer',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
